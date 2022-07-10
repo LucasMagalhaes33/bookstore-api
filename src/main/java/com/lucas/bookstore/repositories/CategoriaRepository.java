5493db88064a2db8586771f7555ab4af1fbb47c4
lucasmagalhaes33 <lucas.magalhaes911@gmail.com>
2022-07-09 21:59:07 -0300

    implementacao dos repositories de livro e categoria

---
+++ src/main/java/com/lucas/bookstore/repositories/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package com.lucas.bookstore.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lucas.bookstore.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+	//comunicação com a base de dados e a persistência das informações
+	
+}
